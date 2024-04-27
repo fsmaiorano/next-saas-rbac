3b4709ae08cf306547c17258a929ca6d78e0fec7
Fábio Maiorano <fsmaiorano@gmail.com>
2024-04-27 15:04:20 +0100

    install fakerjs, create db seed and implement first endpoint

--- apps/api/docs/prisma.md
+++ apps/api/docs/prisma.md
@@ -1,15 +1,31 @@
 # Prisma
 
 ## Instalation
-yarn add -D prisma
-yarn add @prisma/client
-yarn prisma init
+````
+$ yarn add -D prisma
+````
+````
+$ yarn add @prisma/client
+````
+````
+$ yarn prisma init
+````
+## Prisma CLI - Generate client and schema
+````
+$ yarn prisma generate
+````
 
-## Prisma CLI
-yarn prisma generate
-
-## Create migration
-yarn prisma migrate dev --name create-database-structure
+## Create migration and apply
+````
+$ yarn prisma migrate dev --name create-database-structure
+````
 
 ## Apply migration
-yarn prisma migrate dev
+````
+$ yarn prisma migrate dev
+````
+
+## Apply seed (reference in package.json)
+````
+$ yarn prisma db seed
+````
--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post('/users', {
@@ -12,30 +14,29 @@ export async function createAccount(app: FastifyInstance) {
       }),
     },
     handler: async (request, reply) => {
-      return { hello: 'world' }
+      const { name, email, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (userWithSameEmail) {
+        return reply.status(400).send({
+          error: 'Email already in use',
+        })
+      }
+
+      const passwordHash = await hash(password, 10)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send()
     },
   })
 }
-//
-//
-// import type { FastifyInstance } from 'fastify'
-// import type { ZodTypeProvider } from 'fastify-type-provider-zod'
-// import { z } from 'zod'
-//
-// export async function createAccount(app: FastifyInstance) {
-//   app.withTypeProvider<ZodTypeProvider>().post(
-//       '/users',
-//       {
-//         schema: {
-//           body: z.object({
-//             name: z.string(),
-//             email: z.string().email(),
-//             password: z.string().min(6),
-//           }),
-//         },
-//       },
-//       () => {
-//         return { message: 'Account created' }
-//       }
-//   )
-// }

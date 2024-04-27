de271df802670e933f3edeba0ce310ea939e5ff9
Fábio Maiorano <fsmaiorano@gmail.com>
2024-04-27 15:18:50 +0100

    refactor create-account.ts and server.ts to use swagger-ui

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,5 +1,7 @@
 import { fastify } from 'fastify'
 import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
 import { jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod'
 import { createAccount } from './routes/auth/create-account'
 
@@ -8,6 +10,23 @@ const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.register(fastifyCors, {})
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS + RBAC',
+      version: '1.0.0',
+      description:
+        'This project contains all the necessary boilerplate to setup a multi-tenant SaaS with Next.js including authentication and RBAC authorization.',
+    },
+    servers: [{ url: 'http://localhost:3333', description: 'Development server' }],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
 
 app.register(createAccount)
 

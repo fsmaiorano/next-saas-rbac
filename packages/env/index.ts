c3d57a8a0eeed5b254c3fb8505d0c7bfd5916a00
Fábio Maiorano <fsmaiorano@gmail.com>
2024-04-28 12:47:41 +0100

    create package env

---
+++ packages/env/index.ts
@@ -0,0 +1,26 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {
+    SERVER_PORT: z.coerce.number().default(3333),
+    DATABASE_URL: z.string().url(),
+    GITHUB_OAUTH_CLIENT_ID: z.string(),
+    GITHUB_OAUTH_CLIENT_SECRET: z.string(),
+    GITHUB_OAUTH_REDIRECT_URL: z.string(),
+    JWT_SECRET: z.string(),
+    JWT_EXPIRES_IN: z.string(),
+  },
+  client: {},
+  shared: {},
+  runtimeEnv: {
+    SERVER_PORT: process.env.SERVER_PORT,
+    DATABASE_URL: process.env.DATABASE_URL,
+    GITHUB_OAUTH_CLIENT_ID: process.env.GITHUB_OAUTH_CLIENT_ID,
+    GITHUB_OAUTH_CLIENT_SECRET: process.env.GITHUB_OAUTH_CLIENT_SECRET,
+    GITHUB_OAUTH_REDIRECT_URL: process.env.GITHUB_OAUTH_REDIRECT_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+  },
+  emptyStringAsUndefined: true,
+})

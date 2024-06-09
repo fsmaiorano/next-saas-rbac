b6509bdd7692fb74a29f9fc48e80948132fe5415
Fábio Maiorano <fsmaiorano@gmail.com>
2024-06-09 10:35:55 +0100

    setup ky client and implement sign-in-with-password

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,9 +1,14 @@
 'use server'
 
+import { signInWithPassword } from '@/http/sign-in-with-password'
+
 export async function signInWithEmailAndPassword(data: FormData) {
-  console.log(Object.fromEntries(data))
-  // return fetch('/api/auth/sign-in', {
-  //   method: 'POST',
-  //   body: data,
-  // })
+  const { email, password } = Object.fromEntries(data)
+
+  const result = await signInWithPassword({
+    email: String(email),
+    password: String(password),
+  })
+
+  console.log(result)
 }

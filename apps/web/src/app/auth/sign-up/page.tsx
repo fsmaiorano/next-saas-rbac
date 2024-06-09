e1e15dbf2c56f0519b29f636ac82688d915159f3
Fábio Maiorano <fsmaiorano@gmail.com>
2024-06-09 17:51:25 +0100

    implement signup page and actions

--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -1,57 +1,5 @@
-import Image from 'next/image'
-import Link from 'next/link'
-
-import githubIcon from '@/assets/github-icon.svg'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Separator } from '@/components/ui/separator'
+import SignUpForm from '@/app/auth/sign-up/sign-up-form'
 
 export default function SignUpPage() {
-  return (
-    <form action={''} method={''} className={'space-y-4'}>
-      <div className="space-y-1">
-        <Label>Name</Label>
-        <Input type={'text'} name="name" id="name" />
-      </div>
-
-      <div className="space-y-1">
-        <Label>E-mail</Label>
-        <Input type={'email'} name="email" id="email" />
-      </div>
-
-      <div className="space-y-1">
-        <Label>Password</Label>
-        <Input type={'password'} name="password" id="password" />
-      </div>
-
-      <div className="space-y-1">
-        <Label>Confirm your password</Label>
-        <Input
-          type={'password'}
-          name="password-confirmation"
-          id="password-confirmation"
-        />
-      </div>
-
-      <Button type={'submit'} className="w-full">
-        Create account
-      </Button>
-
-      <Button variant="link" className="w-full" asChild>
-        <Link href={'/auth/sign-in'}>Already have an account? Sign in</Link>
-      </Button>
-
-      <Separator />
-
-      <Button type={'submit'} className="w-full" variant="outline">
-        <Image
-          src={githubIcon}
-          alt="GitHub icon"
-          className="mr-2 size-4 dark:invert"
-        />
-        Sign up with GitHub
-      </Button>
-    </form>
-  )
+  return <SignUpForm />
 }
---
+++ apps/web/src/http/sign-up.ts
@@ -0,0 +1,22 @@
+import { api } from './api-client'
+
+interface SignUpRequest {
+  name: string
+  email: string
+  password: string
+}
+
+type SignUpResponse = void
+export async function signUp({ name, email, password }: SignUpRequest) {
+  const result = await api.post('users', {
+    json: {
+      name,
+      email,
+      password,
+    },
+  })
+
+  console.log(result)
+
+  return result
+}

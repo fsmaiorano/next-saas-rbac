2c07dce68e933e71e6dc8a7da73f66e59ea43470
Fábio Maiorano <fsmaiorano@gmail.com>
2024-06-10 14:19:07 +0100

    implement create-organization sheet

---
+++ apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
@@ -0,0 +1,19 @@
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet'
+// import { OrganizationForm } from '@/app/(app)/create-organization/organization-form'
+import { OrganizationForm } from '../../create-organization/organization-form'
+
+export default function CreateOrganization() {
+  return (
+    <Sheet defaultOpen>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Create organization</SheetTitle>
+        </SheetHeader>
+
+        <div className="py-4">
+          <OrganizationForm />
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
+}
